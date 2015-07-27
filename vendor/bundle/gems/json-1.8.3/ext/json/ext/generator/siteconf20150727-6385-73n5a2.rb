6083ae7aed686687d9a1158c46b133f5b609c8e1
marcus <marcuslin0724@.gmail.com>
2015-07-27 23:48:53 +0800

    add jekyll

---
+++ vendor/bundle/gems/json-1.8.3/ext/json/ext/generator/siteconf20150727-6385-73n5a2.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150727-6385-1lgyc4m"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
---
+++ vendor/bundle/gems/nokogiri-1.6.6.2/ext/nokogiri/siteconf20150727-6383-17d37uq.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150727-6383-162p4eh"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
